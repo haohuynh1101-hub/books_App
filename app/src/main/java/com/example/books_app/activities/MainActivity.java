8473a143f3787ffc454f0810d4dc27e9ded227d8
Hao Huynh <haohuynh1101-hub>
2021-10-24 21:52:19 +0700

    create fragment

---
+++ app/src/main/java/com/example/books_app/activities/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.books_app.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.books_app.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+    private ActionBar actionBar;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        addControls();
+        addEvents();
+
+    }
+
+    private void addControls() {
+        bottomNavigationView=findViewById(R.id.bottom_navigation);
+    }
+
+    private void addEvents() {
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, new home()).commit();
+
+    }
+
+    private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectFragment = null;
+            switch (item.getItemId()) {
+                case R.id.nav_home:
+                    selectFragment = new home();
+
+                    break;
+                case R.id.nav_cart:
+                    selectFragment = new cart();
+                    break;
+                case R.id.nav_category:
+                    selectFragment = new category();
+                    break;
+                case R.id.nav_profile:
+                    selectFragment = new profile();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.frame_container, selectFragment).commit();
+            return true;
+        }
+    };
+}
